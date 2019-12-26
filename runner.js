1b08cf3b0532a6b5c28ba25f0d03e142e77f6ed5
mlengse <anjangkn@gmail.com>
2019-12-26 07:28:18 +0700

    tglLength

--- runner.js
+++ runner.js
@@ -23,7 +23,7 @@ module.exports = async () => {
 
           let { tglLength, tglSum, bln, blnNum, thn } = ekin.tgl[a]
 
-          let maxPoin = Math.round(8500*(tglLength/(tglSum - 5)))
+          let maxPoin = Math.round(8500*((tglLength > 20 ? 20: tglLength)/(tglLength > 20 ? 20 : (tglSum - 5))))
 
           console.log(tglLength, tglSum, blnNum, thn, maxPoin)
 
